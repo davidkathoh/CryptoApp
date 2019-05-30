6d513850f0fbc58898fcfb34eb120d86f7c04570
davidKathoh <mudavidkat@gmail.com>
2019-05-30 20:14:51 +0200

    detail fragment implemented

--- app/src/main/java/com/yoonek/cryptoapp/cryptodetail/CryptoDetailFragment.kt
+++ app/src/main/java/com/yoonek/cryptoapp/cryptodetail/CryptoDetailFragment.kt
@@ -1,17 +1,29 @@
 package com.yoonek.cryptoapp.cryptodetail
 
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProviders
 
 import com.yoonek.cryptoapp.R
+import com.yoonek.cryptoapp.database.Crypto
+import kotlinx.android.synthetic.main.fragment_crypto_detail.*
 
 
 class CryptoDetailFragment : Fragment() {
+
+    private lateinit var bigPrice:TextView
+    private lateinit var symbol:TextView
+    private lateinit var price:TextView
+    private lateinit var change1:TextView
+    private lateinit var change7:TextView
+    private lateinit var change24:TextView
+    private lateinit var update:TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,7 +36,53 @@ class CryptoDetailFragment : Fragment() {
         val viewModelFactory = CryptoDetailViewModel.CryptoDetailViewModelFactory(cryptoName)
         val viewModel = ViewModelProviders.of(this,viewModelFactory).get(CryptoDetailViewModel::class.java)
 
+        inits(view)
+        bind(viewModel.crypto)
+
         return view
+    }
+
+    private  fun inits(view: View){
+        bigPrice = view.findViewById(R.id.cr_big_price)
+        price = view.findViewById(R.id.cr_price)
+        symbol = view.findViewById(R.id.cr_symbol)
+        change1 = view.findViewById(R.id.cr_change1)
+        change7 = view.findViewById(R.id.cr_change7)
+        change24 = view.findViewById(R.id.cr_change24)
+        update = view.findViewById(R.id.cr_update)
+
+    }
+
+
+    private fun bind(crypto: Crypto?){
+        crypto?.let {
+            bigPrice.text = getString(R.string.price_frmt,crypto.price_usd)
+            price.text =  getString(R.string.price_frmt,crypto.price_usd)
+            symbol.text =crypto.symbol
+            change24.text = crypto.percent_change_24h
+            change7.text = crypto.percent_change_7d
+            change1.text = crypto.percent_change_1h
+
+
+            change1.setTextColor(if (crypto.percent_change_1h.contains("-"))
+                Color.parseColor(getString(R.string.redText))
+            else
+                Color.parseColor(getString(R.string.greenText)))
+
+            change7.setTextColor(if (crypto.percent_change_7d.contains("-"))
+                Color.parseColor(getString(R.string.redText))
+            else
+                Color.parseColor(getString(R.string.greenText)))
+
+            change24.setTextColor(if (crypto.percent_change_24h.contains("-"))
+                Color.parseColor(getString(R.string.redText))
+            else
+                Color.parseColor(getString(R.string.greenText)))
+
+
+
+
+        }
     }
 
 
